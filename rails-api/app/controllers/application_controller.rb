c35a99f6ae1312d08a34c433812c6f5bee52d71a
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-26 15:54:38 +0900

    apiのテスト用のパスを作った。

--- rails-api/app/controllers/application_controller.rb
+++ rails-api/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    def health_check
+        render json: { status: 'ok' }
+    end
 end
--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/health_check', to: 'application_controller#health_check'
 end
