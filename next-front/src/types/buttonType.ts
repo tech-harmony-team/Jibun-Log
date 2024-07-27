3912f195fe2127b800ed699a166b78a9a111d461
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2024-07-27 15:50:23 +0900

    [FE#3]: CircleButtonにonClickハンドラを渡せるように変更

--- next-front/src/types/buttonType.ts
+++ next-front/src/types/buttonType.ts
@@ -4,5 +4,5 @@ export type ButtonProps = {
 	color: string;
 	backgroundColor?: string;
 	Icon: React.ElementType;
-  // onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  onClick?: () => void;
 };
