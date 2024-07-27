17251995c6c4a0042e07daf99c4055cc9fef2b6e
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-27 13:00:32 +0900

    react-cookieを用いて、cookieを保存・取得できることを確認した。

--- next-front/src/app/cookie-test/page.tsx
+++ next-front/src/app/cookie-test/page.tsx
@@ -3,56 +3,28 @@
 import { useState } from "react";
 
 import { useUserDatas } from "@/hooks/useUserDatas";
-import { UserData } from "@/types/userData";
 
 export default function CookieTestPage() {
-  const [savedData, setSavedData] = useState<UserData | null>(null);
-  const { setUserData, removeUserData, getUserData } = useUserDatas();
-
-  const setUserDataCookie = () => {
-    const userData: UserData = {
-      id: 1,
-      email: "test@example.com",
-      response: "分析結果",
-      accessToken: "XXXXXXXXX",
-      client: "YYYYYYYYY",
-      uid: "ZZZZZZZZZ",
-    };
-
-    setUserData(userData);
-  };
-
-  const getUserDataCookie = () => {
-    const userData = getUserData();
-    if (!userData.id) {
-      console.log("データがありません");
-      setSavedData(null);
-      return;
-    }
-    console.log("結果" + JSON.stringify(userData));
-    setSavedData(userData);
-  };
-
-  const showUserData = () => {
-    console.log(savedData);
-  };
-
-  const removeUserDataCookie = () => {
-    removeUserData();
-  };
+  const [name, setName] = useState<string>("");
+  const [savedName, setSavedName] = useState<string>("");
+  const { setUserName, removeUserName, getUserName } = useUserDatas();
 
   return (
     <div>
       <h1>Cookeの動作確認</h1>
-      <button onClick={setUserDataCookie}>保存</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={() => setUserName(name)}>保存</button>
       <br />
 
-      <button onClick={getUserDataCookie}>取得</button>
+      <button onClick={() => setSavedName(getUserName())}>取得</button>
       <br />
+      <p>保存されているデータ：{savedName}</p>
 
-      <button onClick={showUserData}>表示</button>
-
-      <button onClick={removeUserDataCookie}>削除</button>
+      <button onClick={removeUserName}>削除</button>
     </div>
   );
 }
