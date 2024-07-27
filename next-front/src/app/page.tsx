0fb46bf39c3e9103322d489b38e58938332800a4
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-27 18:37:39 +0900

    テスト仕様だったトップ画面を書き換えた。

--- next-front/src/app/page.tsx
+++ next-front/src/app/page.tsx
@@ -1,21 +1,17 @@
 "use client";
 
-import { Button } from "@mui/material";
-import { CircleButton } from "../components/button/circleButton";
-import { CheckRounded, AddRounded, FavoriteRounded, ChevronRightRounded} from '@mui/icons-material';
-
 export default function Home() {
-	const handleClick = () => {
-		console.log("clicked");
-	}
+  const handleClick = () => {
+    console.log("clicked");
+  };
 
   return (
     <>
-      <h1 className="text-red-600 text-4xl font-bold">自分Log</h1>
-	  	<CircleButton label="登録" fontsize="50px" color="#fff" backgroundColor="#1A85D1" Icon={CheckRounded} />
-		<CircleButton label="日記を書く" fontsize="50px" color="#fff" backgroundColor="#60D11A" Icon={AddRounded} />
-		<CircleButton label="お気に入り" fontsize="50px" color="#fff" backgroundColor="#F08810" Icon={FavoriteRounded} />
-		<CircleButton label="次へ" fontsize="50px" color="#fff" backgroundColor="#60D11A" Icon={ChevronRightRounded} onClick={handleClick} />
+      <div className="w-4/5 max-w-600 h-full mx-auto pt-16 bg-stone-50">
+        <h1 className="text-blue-500 text-center text-4xl font-bold">
+          自分Log
+        </h1>
+      </div>
     </>
   );
 }
