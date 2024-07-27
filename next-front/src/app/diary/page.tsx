28e22b614b386e48edbe6cb9ab4c9f69f1d87431
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-27 17:43:35 +0900

    画面のデザインを調整した。

--- next-front/src/app/diary/page.tsx
+++ next-front/src/app/diary/page.tsx
@@ -1,8 +1,12 @@
+import PageHeader from "@/components/typography/pageHeader";
+
 export default function DiaryPage() {
   return (
-    <div className="w-4/5 h-full mx-auto pt-16 bg-yellow-300">
-      <h1 className="text-4xl text-left mb-5">日記</h1>
-      <div className="h-4/5 py-5 flex flex-col items-center bg-red-200">
+    <div className="w-4/5 max-w-600 h-full mx-auto pt-14 bg-stone-50">
+      <PageHeader type="h1" textAlignment="left">
+        日記
+      </PageHeader>
+      <div className="h-4/5 py-5 flex flex-col items-center">
         <div>日記1</div>
         <div>日記2</div>
         <div>日記3</div>
