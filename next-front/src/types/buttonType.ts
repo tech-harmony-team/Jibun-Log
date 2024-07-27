9ce2ec1008101a6ad53cf0bacb8bbb88998c5b68
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2024-07-27 17:53:51 +0900

    [FE#3] 背景なしボタンに統一, テンプレをtypes/buttonType.tsにメモ書き

--- next-front/src/types/buttonType.ts
+++ next-front/src/types/buttonType.ts
@@ -6,3 +6,42 @@ export type ButtonProps = {
 	Icon: React.ElementType;
   onClick?: () => void;
 };
+
+// template
+
+// const checkIcon: React.FC<ButtonProps> = () => (
+//   <CircleButton
+//     label="登録"
+//     fontsize="50px"
+//     color="#fff"
+//     backgroundColor="#1A85D1"
+//     Icon={CheckRounded}
+//   />
+// )
+
+// const addIcon: React.FC<ButtonProps> = () => (
+// 	<CircleButton
+// 		label="日記を書く"
+// 		fontsize="50px"
+// 		color="#fff"
+// 		backgroundColor="#60D11A"
+// 		Icon={AddRounded} />
+// )
+
+// const favoriteIcon: React.FC<ButtonProps> = () => (
+// 	<CircleButton
+// 		label="お気に入り"
+// 		fontsize="50px"
+// 		color="#fff"
+// 		backgroundColor="#F08810"
+// 		Icon={FavoriteRounded} />
+// )
+
+// const nextIcon: React.FC<ButtonProps> = () => (
+// 	<CircleButton
+// 		label="次へ"
+// 		fontsize="50px"
+// 		color="#fff"
+// 		backgroundColor="#60D11A"
+// 		Icon={ChevronRightRounded} />
+// )
