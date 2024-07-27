533087b16739979d9f2c7b115813ae05f660d310
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-27 17:07:26 +0900

    軽いレイアウトを作成した。

---
+++ next-front/src/app/diary/page.tsx
@@ -0,0 +1,12 @@
+export default function DiaryPage() {
+  return (
+    <div className="w-4/5 h-full mx-auto bg-yellow-300">
+      <h1 className="text-4xl text-center">日記</h1>
+      <div className="flex flex-col justify-center items-center">
+        <div>日記1</div>
+        <div>日記2</div>
+        <div>日記3</div>
+      </div>
+    </div>
+  );
+}
