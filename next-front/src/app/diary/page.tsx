3db3704e94ea68c73e60d48a8a4f793024819c8f
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-27 17:33:43 +0900

    レイアウトを調整した。

--- next-front/src/app/diary/page.tsx
+++ next-front/src/app/diary/page.tsx
@@ -1,8 +1,8 @@
 export default function DiaryPage() {
   return (
-    <div className="w-4/5 h-full mx-auto bg-yellow-300">
-      <h1 className="text-4xl text-center">日記</h1>
-      <div className="flex flex-col justify-center items-center">
+    <div className="w-4/5 h-full mx-auto pt-16 bg-yellow-300">
+      <h1 className="text-4xl text-left mb-5">日記</h1>
+      <div className="h-4/5 py-5 flex flex-col items-center bg-red-200">
         <div>日記1</div>
         <div>日記2</div>
         <div>日記3</div>
