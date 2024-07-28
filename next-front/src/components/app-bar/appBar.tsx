8ec86db046b17602ba5c4327d9d60820d6ed07ad
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-28 10:56:16 +0900

    メニューを作成して、開閉できるようにした。

--- next-front/src/components/app-bar/appBar.tsx
+++ next-front/src/components/app-bar/appBar.tsx
@@ -1,18 +1,55 @@
+"use client";
+
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 
 export default function AppBar() {
+  const [menuOpen, setMenuOpen] = useState(true);
+
   return (
     <div>
-      <div className="w-full h-12 py-2 px-5 flex flex-row justify-center items-center bg-white drop-shadow-md z-50 fixed">
+      <div className="w-full h-12 py-2 px-5 flex flex-row justify-center items-center bg-white drop-shadow-md z-40 fixed">
         <div className="w-3/4 h-full flex items-center">
           <h1 className="text-3xl font-bold">自分.log</h1>
         </div>
-        <div className="w-1/4 h-full flex items-center justify-end">
-          <button>
-            <PersonOutlineIcon fontSize="large" />
-          </button>
+        <div
+          className="w-1/4 h-full flex items-center justify-end cursor-pointer"
+          onClick={() => setMenuOpen(true)}
+        >
+          <PersonOutlineIcon fontSize="large" />
         </div>
       </div>
+      {/* メニュー */}
+      {menuOpen && (
+        <div className=" fixed top-0 left-0 w-full h-full z-50 bg-white">
+          <div className="w-full h-20 py-2 px-5 flex flex-row justify-end">
+            <div
+              className="w-full h-auto flex justify-end items-end cursor-pointer"
+              onClick={() => setMenuOpen(false)}
+            >
+              <CloseIcon fontSize="large" />
+            </div>
+          </div>
+          <div className="w-3/4 h-full flex flex-col gap-5 mx-auto my-10">
+            <h1 className="text-blue-500 mb-5 text-center text-4xl font-bold">
+              自分.Log
+            </h1>
+            <div className="w-full h-10 flex justify-center items-center cursor-pointer">
+              <h2 className="text-2xl">ホーム</h2>
+            </div>
+            <div className="w-full h-10 flex justify-center items-center cursor-pointer">
+              <h2 className="text-2xl">日記</h2>
+            </div>
+            <div className="w-full h-10 flex justify-center items-center cursor-pointer">
+              <h2 className="text-2xl">分析</h2>
+            </div>
+            <div className="w-full h-10 flex justify-center items-center cursor-pointer">
+              <h2 className="text-2xl">設定</h2>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
