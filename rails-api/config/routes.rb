9ae2c89c523422b8f3589d641d69aab1e96f0f21
mametaro99 <bearsmichimato99@gmail.com>
2024-07-28 13:12:24 +0900

    originalityのカラムの修正と、Dairyコントローラーのディレクトリを移動しました。

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,8 +7,9 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
+        mount_devise_token_auth_for 'User', at: 'auth'
         namespace :current do
-          resources :diaries, only: [:index, :show, :create]
+          resources :diaries, only: [:index, :show, :create, :update, :delete]
         end
       end
     end
