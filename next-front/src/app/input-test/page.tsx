5848e94b1ed0260c002ab29419b9293b8bf07580
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2024-07-28 12:17:03 +0900

    [FE#10] 1ページ完結に変更、valueの取得も可能

--- next-front/src/app/input-test/page.tsx
+++ next-front/src/app/input-test/page.tsx
@@ -1,47 +1,50 @@
 "use client";
 
-import { useForm, SubmitHandler } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { TextInput } from "@/components/textInput/textInput";
-import { useCallback } from "react";
+import { useState } from "react";
 import PageHeader from "@/components/typography/pageHeader";
 import { CircleButton } from "@/components/button/circleButton";
-import { ChevronRightRounded, ArrowBackIosNew } from "@mui/icons-material";
+import { ChevronRightRounded, ArrowBackIosNew, CheckRounded } from "@mui/icons-material";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
 
-const schema = z.object({
-	title: z.string().min(1, "タイトルを入力してください"),
-	diary: z.string().min(1, "本文を入力してください"),
-	effort: z.string().min(1, "工夫を入力してください"),
-	gain: z.string().min(1, "得たことを入力してください"),
-});
-
-export type Schema = z.infer<typeof schema>;
 
 export default function InputPage() {
-	const { control, handleSubmit, reset, formState: {errors} } = useForm<Schema>({
-		resolver: zodResolver(schema),
-		defaultValues: {
-			title: "",
-			diary: "",
-			effort: "",
-			gain: "",
-		},
-	});
+	const [title, setTitle] = useState<string>("");
+	const [diary, setDiary] = useState<string>("");
+	const [effort, setEffort] = useState<string>("");
+	const [gain, setGain] = useState<string>("");
+	const [date, setDate] = useState<Date>();
+	const [startTime, setStartTime] = useState<Date>();
+	const [endTime, setEndTime] = useState<Date>();
+	const [allDay, setAllDay] = useState(false);
+
+	const [error, setError] = useState<boolean>(false);
 
 	const handleBack = () => {
 		window.location.href = '/home';
 	}
 
-	const onSubmit: SubmitHandler<Schema> = useCallback((data) => {
-		console.log(data);
-		window.location.href = '/input-test-2';
-		}, []);
-
-	const handleReset = useCallback(() => {
-		reset();
-	}, [reset]);
-
+	const handleSubmit =  () => {
+		if (!title || !diary || !effort || !gain) {
+			setError(true);
+			return;
+		}
+		if (!date || !(allDay || (startTime && endTime))) {
+			alert("日付、開始時間、終了時間は必須です");
+			return;
+		}
+		setError(false);
+		// console.log(title);
+		// console.log(diary);
+		// console.log(effort);
+		// console.log(gain);
+		// console.log(date);
+		// console.log(startTime);
+		// console.log(endTime);
+		// console.log(allDay? "終日": "終日ではない");
+		window.location.href = '/input-test-3';
+	}
+	
 	return (
 		<>
 		<div className="w-4/5 max-w-600 h-full mx-auto pt-14 bg-stone-50 overflow-auto">
@@ -51,14 +54,75 @@ export default function InputPage() {
 			<PageHeader type="h2" textAlignment="left">
 				日記を書く 1/2
 			</PageHeader>
-			<form className="flex flex-col w-4/5 mx-auto space-y-4" onSubmit={handleSubmit(onSubmit)}>
-				<TextInput control={control} id="outlined-required" name="title" label="タイトル" fullWidth multiline rows={2} margin="normal"/>
-				<TextInput control={control} id="outlined-required" name="diary" label="本文" fullWidth multiline rows={5} margin="normal"/>
-				<TextInput control={control} id="outlined-required" name="effort" label="工夫" fullWidth multiline rows={5} margin="normal"/>
-				<TextInput control={control} id="outlined-required" name="gain" label="得たこと" fullWidth multiline rows={5} margin="normal"/>
+			<div className="flex flex-col w-4/5 mx-auto space-y-4">
+				<div className="flex flex-row space-x-4">
+					<label>日付</label>
+					<DatePicker
+						selected={date}
+						onChange={(date) => setDate(date!)}
+						showDateSelect
+						dateFormat={"MM/dd"}
+					/>
+				</div>
+				<div className="flex flex-row space-x-4">
+					<label>開始時間</label>
+					<DatePicker
+						selected={startTime}
+						onChange={(date) => setStartTime(date!)}
+						showTimeSelect
+						showTimeSelectOnly
+						timeIntervals={15}
+						timeCaption="Time"
+						dateFormat="HH:mm"
+					/>
+				</div>
+				<div className="flex flex-row space-x-4">
+					<label>終了時間</label>
+					<DatePicker
+						selected={endTime}
+						onChange={(date) => setEndTime(date!)}
+						showTimeSelect
+						showTimeSelectOnly
+						timeIntervals={15}
+						timeCaption="Time"
+						dateFormat="HH:mm"
+					/>
+				</div>
+				<div className="flex flex-row space-x-4">
+					<label>終日に設定</label>
+					<input type="checkbox" checked={allDay} style={{ width: '20px', height: '20px' }} onChange={() => setAllDay(!allDay)} />
+				</div>
+			<div className="flex flex-col w-full mx-auto space-y-4">
+				<input
+					type="text"
+					className="w-full p-2 border border-gray-300 rounded-md"
+					placeholder="タイトル"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<textarea
+					className="w-full p-2 border border-gray-300 rounded-md"
+					placeholder="本文"
+					value={diary}
+					rows={4}
+					onChange={(e) => setDiary(e.target.value)}
+				/>
+				<textarea
+					className="w-full p-2 border border-gray-300 rounded-md"
+					placeholder="工夫したこと"
+					value={effort}
+					rows={4}
+					onChange={(e) => setEffort(e.target.value)}
+				/>
+				<textarea
+					className="w-full p-2 border border-gray-300 rounded-md"
+					placeholder="得たこと"
+					value={gain}
+					rows={4}
+					onChange={(e) => setGain(e.target.value)}
+				/>
+				{error && <p className="text-red-500">すべて入力してください。</p>}
 				<div className="flex flex-row justify-end space-x-4">
-					{/* フォームのリセット用ボタン */}
-					{/* <button type="button" onClick={handleReset}>Reset</button> */}
 					<CircleButton
 						label="戻る"
 						fontsize="50px"
@@ -72,11 +136,12 @@ export default function InputPage() {
 						fontsize="50px"
 						color="#fff"
 						backgroundColor="#60D11A"
-						Icon={ChevronRightRounded} 
-						onClick={handleSubmit(onSubmit)}
+						Icon={ChevronRightRounded}
+						onClick={handleSubmit}
 					/>
 				</div>
-			</form>
+			</div>
+			</div>
     </div>
 		</>
 	);
